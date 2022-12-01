fd86f630d06da56b5053e8d86e00190bb0b2f4a2
santiago-matiz <118410232+santiago-matiz@users.noreply.github.com>
2022-12-01 14:10:24 -0500

    Update Ejercicio 1.sql

--- Modulo 4/Ejercicio 1.sql
+++ Modulo 4/Ejercicio 1.sql
@@ -1,9 +1,7 @@
 SELECT 
-Year(date(dt)) as year,
-Month(date(dt)) as month,
-min(date(dt))as First_date,
+date_trunc('MONTH',date(dt)) as date,
 count(distinct act_acct_cd) as Number_of_clients
 FROM "db-analytics-prod"."fixed_cwp"
 Where act_cust_typ_nm ='Residencial' and extract(year from date(dt))=2022 and date(dt)=date_trunc('MONTH',date(dt))
-Group by 1,2
-Order by month
+Group by 1
+Order by date
